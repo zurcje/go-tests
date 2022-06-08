d19a5225c1962903084d4db8c9fe537ef740eef3
zurcje <jwdcruz@gmail.com>
2022-06-08 08:16:01 -0300

    Sub test

---
+++ go-testing/calc.go
@@ -0,0 +1,9 @@
+// Para a função Sub() visto na aula, execute o teste de unidade correspondente. Para isto:
+// 1. Dentro da pasta go-testing crie um arquivo calc.go com a função para testar.
+// 2. Dentro da pasta go-testing, crie um arquivo calc_test.go com o teste projetado.
+
+package calc
+
+func Sub(num1, num2 int) int {
+	return num1 - num2
+}
